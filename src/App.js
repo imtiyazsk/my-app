816ec3672cfb0a54b35ceed580f015c90f614f41
imityazsk <imtiyazsk153@gmail.com>
2022-10-19 23:32:08 +0530

    some

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
+// import About from './Components/About';
 import Navbar from'./Components/Navbar';
 import TextForm from'./Components/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+
+  const[mode,setMode] = useState("light");
+  const toggleMode= ()=>{
+    if(mode === "light"){
+        setMode("dark");
+        document.body.style.backgroundColor = '#042743';
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
    <>
-    <Navbar title="TextUtils"/>
-    <TextForm heading="Enter The Text To Analyze"/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <TextForm heading="Enter The Text To Analyze" mode={mode}/>
+    {/* <About/> */}
     
    </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -14,26 +14,42 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText)
     }
+    const handelClear = ()=>{
+      let newText = "";
+      setText(newText)
+  }
+  const clearExtra = ()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+}
+const handelCopy= ()=>{
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+}
 
     
 
-    const[text,setText] = useState(" ")
+    const[text,setText] = useState('')
   return (
     <div>
-      <div className="container">
+      <div className="container" style={{color: props.mode==="dark"?"white":"#042743"}}>
+      <h1 className="mt-3">{props.heading}</h1>
         <div className="mb-3">
-          <h1 className="mt-3">{props.heading}</h1>
-          <textarea className="form-control mt-3" value={text}  onChange={handelOnChange} id="myBox" rows="8 "></textarea>
+          <textarea className="form-control mt-3" value={text}  onChange={handelOnChange} style={{backgroundColor: props.mode==="dark"?"grey":"white", color: props.mode==="dark"?"white":"#042743"}} id="myBox" rows="8 "></textarea>
           <button className="btn btn-primary mt-3" onClick={handelUpClick}>Convert To UpperCase</button>
           <button className="btn btn-primary mt-3 ms-5" onClick={handel_UpClick}>Convert To LowerCase</button>
+          <button className="btn btn-primary mt-3 ms-5" onClick={handelClear}>Clear</button>
+          <button className="btn btn-primary mt-3 ms-5" onClick={clearExtra}>Clear Extra Space</button>
+          <button className="btn btn-primary mt-3 ms-5" onClick={handelCopy}>Copy</button>
         </div>
       </div>
-      <div className="container">
+      <div className="container" style={{color: props.mode==="dark"?"white":"#042743"}}>
         <h2>Your Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length}Minutes to Read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something In The Above Textbox To preview Here"}</p>
       </div>
     </div>
   );
